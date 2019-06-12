ef34a126ae44290c3c7e41f418567af43e91810f
afigurekim <afigurekim@gmail.com>
2019-06-12 12:42:39 +0900

    form test

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -1,21 +1,48 @@
-alert('자바스크립트 연결 성공 !!');
-
-var wrapper = document.querySelector('#wrapper');
-wrapper.innerHTML = '<h1>SPA 시작</h1>'
-+'<div id="content">삭제할 내용</div>'
-+'<button id="btn">지워버리자</button>'
-+'<button id="btn2">추가하자</button>';
-
-var btn = document.querySelector('#btn');
-var content = document.querySelector('#content');
-
-btn.addEventListener('click', function(){
-    content.innerHTML = '';
-});
-
-var btn2 = document.querySelector('#btn2');
-
-btn2.addEventListener('click', function() {
-    var temp = document.createTextNode('새로 추가됨');
-    content.appendChild(temp);
-});
+var app = (function(){
+    let init = function(){
+        login_form();
+    }
+    let login_form = function(){
+        var wrapper = document.querySelector('#wrapper');
+        wrapper.innerHTML = '<form action="/action_page.php">'
+        +'  First name:<br>'
+        +'  <input type="text" name="firstname" value="Mickey">'
+        +'  <br>'
+        +'  Last name:<br>'
+        +'  <input type="text" name="lastname" value="Mouse">'
+        +'  <br><br>'
+        +'  <input id="btn1" type="submit" value="로그인">'
+        +'  <input id="btn2" type="submit" value="회원가입">'
+        +'</form> ';
+    
+        var btn2 = document.querySelector('#btn2');
+    
+        btn2.addEventListener('click', function(){
+            join_form();
+        });
+    }
+    let join_form = function(){
+        //var wrapper = document.querySelector('#wrapper');
+        wrapper.innerHTML = '<form>'
+        +'	ID<br>'
+        +'	<input type="text" name="id"><br>'
+        +'	PW<br>'
+        +'	<input type="password" name="pw"><br>'
+        +'	이름<br>'
+        +'	<input type="text" name="name"><br>'
+        +'	주민번호<br>'
+        +'	<input type="password" name="ssn"><br>'
+        +'	전화번호<br>'
+        +'	<input type="text" name="phone"><br><br>'
+        +'	<input id="btn3" type="submit" value="확인">'
+        +'	<input id="btn4" type="reset" value="취소">'
+        +'</form>';
+    
+        var btn3 = document.querySelector('#btn3');
+    
+        btn3.addEventListener('click', function() {
+            login_form();
+        });
+    };
+    return {init : init};
+})();
